cb5b825f1083b7b08faddd84852e3f9bddb62ccb
uzorbenny-beep <uzorbenny51@gmail.com>
2026-05-31 15:03:39 +0100

    Add auth store

---
+++ stores/authStore.ts
@@ -0,0 +1,20 @@
+'use client'
+
+import { create } from 'zustand'
+import { User } from '@/types'
+
+interface AuthStore {
+  user: User | null
+  isLoading: boolean
+  setUser: (user: User | null) => void
+  setLoading: (loading: boolean) => void
+  logout: () => void
+}
+
+export const useAuthStore = create<AuthStore>((set) => ({
+  user: null,
+  isLoading: true,
+  setUser: (user) => set({ user }),
+  setLoading: (loading) => set({ isLoading: loading }),
+  logout: () => set({ user: null }),
+}))
